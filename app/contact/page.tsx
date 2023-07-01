90651a0d12335dd6789aab313afd8e4874b8a079
Cristian Ojeda <cristianojeda.maimo@gmail.com>
2023-07-01 10:00:44 -0300

    Projects and Contact Page with data from json

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,20 +1,22 @@
 import React from 'react'
+import data from '@/components/data/data.json'
 
 export const ContactPage: React.FC = () => {
+  const { title, inputName, inputEmail, inputMessage, cta } = data.contact
   return (
     <section className='flex flex-col items-center mt-10 mb-36' id='projects'>
-            <h1 className='text-5xl font-bold leading-normal'>Contact</h1>
-            <span className='bg-brand-yellow w-24 h-1 rounded-sm'></span>
-            <form className='flex flex-col text-brand-grayHigh'>
-                <label htmlFor="name" className='text-base mb-2'>Name</label>
-                <input type="text" name='name' id='name' className='rounded-lg bg-white mb-6 border border-gray-200 w-96 h-10 px-4 focus:border-brand-yellow focus:border-2 focus:outline-none' />
-                <label htmlFor="email" className='text-base mb-2'>Email</label>
-                <input type="text" name='email' id='email' className='rounded-lg bg-white mb-6 border border-gray-200 w-96 h-10 px-4 focus:border-brand-yellow focus:border-2 focus:outline-none' />
-                <label htmlFor="message" className='text-base mb-2'>Message</label>
-                <textarea name='message' id='message' className='rounded-lg bg-white mb-6 border border-gray-200 w-96 h-40 px-4 py-2 resize-none focus:border-brand-yellow focus:border-2 focus:outline-none' />
-                <button className='bg-brand-yellow px-6 py-2 rounded-lg self-end text-lg'>Send</button>
-            </form>
-        </section>
+        <h1 className='text-5xl font-bold leading-normal'>{title}</h1>
+        <span className='bg-brand-yellow w-24 h-1 rounded-sm'></span>
+        <form className='flex flex-col text-brand-grayHigh'>
+            <label htmlFor="name" className='text-base mb-2'>{inputName}</label>
+            <input type="text" name='name' id='name' className='rounded-lg bg-white mb-6 border border-gray-200 w-96 h-10 px-4 focus:border-brand-yellow focus:border-2 focus:outline-none' />
+            <label htmlFor="email" className='text-base mb-2'>{inputEmail}</label>
+            <input type="text" name='email' id='email' className='rounded-lg bg-white mb-6 border border-gray-200 w-96 h-10 px-4 focus:border-brand-yellow focus:border-2 focus:outline-none' />
+            <label htmlFor="message" className='text-base mb-2'>{inputMessage}</label>
+            <textarea name='message' id='message' className='rounded-lg bg-white mb-6 border border-gray-200 w-96 h-40 px-4 py-2 resize-none focus:border-brand-yellow focus:border-2 focus:outline-none' />
+            <button className='bg-brand-yellow px-6 py-2 rounded-lg self-end text-lg'>{cta}</button>
+        </form>
+    </section>
   )
 }
 
--- components/Card.tsx
+++ components/Card.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image'
+import data from '@/components/data/data.json'
 import React from 'react'
+
 interface CardProps {
   title: string
   desc: string
@@ -8,12 +10,13 @@ interface CardProps {
 }
 
 export const Card: React.FC<CardProps> = ({ title, desc, img, odd = false }) => {
+  const { cta } = data.projects
   return (
         <article className={`mt-20 flex rounded-3xl bg-white justify-between ${odd ? 'flex-row-reverse' : ''} items-center w-4/5`}>
             <aside className='px-12'>
                 <h3 className='text-brand-grayHigh text-4xl font-bold leading-normal'>{ title }</h3>
                 <p className='text-lg font-normal leading-normal text-brand-grayMedium py-6'>{ desc }</p>
-                <button className='px-6 py-2 border border-brand-grayHigh leading-normal text-lg rounded-3xl'>View Project</button>
+                <button className='px-6 py-2 border border-brand-grayHigh leading-normal text-lg rounded-3xl'>{ cta }</button>
             </aside>
             <Image src={`/images/${img}`} width={496} height={526} alt='project1' className={odd ? 'rounded-s-3xl' : 'rounded-e-3xl'} />
         </article>
